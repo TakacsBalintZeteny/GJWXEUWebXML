f126c033e8d45bacdbcd9a4031f788ec87340105
Takács Bálint Zétény <146757364+TakacsBalintZeteny@users.noreply.github.com>
2025-10-29 11:24:50 +0100

    Add files via upload

---
+++ GJWXEU_1029/1. feladat/DOMRead.java
@@ -0,0 +1,19 @@
+package domgjwxeu1029;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class DOMRead {
+
+    
+}
